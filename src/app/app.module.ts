5b981050b5eeea3d8dbbe0d6f6b08c010bf504bf
shashank_a <shashankappana@gmail.com>
2021-05-08 16:18:07 +0530

    changing it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { ProductOneComponent } from './product-one/product-one.component';
-import { ProductTwoComponent } from './product-two/product-two.component';
-import { ProductThreeComponent } from './product-three/product-three.component';
-import { ProductFourComponent } from './product-four/product-four.component';
-import { ProductFiveComponent } from './product-five/product-five.component';
-import { ProductSixComponent } from './product-six/product-six.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductOneComponent,
-    ProductTwoComponent,
-    ProductThreeComponent,
-    ProductFourComponent,
-    ProductFiveComponent,
-    ProductSixComponent
+    ProductOneComponent
   ],
   imports: [
     BrowserModule,
